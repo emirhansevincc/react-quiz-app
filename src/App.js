a27f1db818d5302fbb448142499f3d65699aadd6
emirhan sevinc <selin_7383@hotmail.com>
2022-07-19 23:03:08 +0300

    Added function to change question

--- src/App.js
+++ src/App.js
@@ -4,24 +4,61 @@ import { questions } from './questions';
 
 function App() {
 
-  const [currentQuestion, setcurrentQuestion] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [score, setScore] = useState(0)
+  const [finish, setFinish] = useState(false)
+
+  const nextQuestion = (isCorrect) => {
+
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+
+    if ((currentQuestion + 1) < questions.length) {
+      setCurrentQuestion(currentQuestion + 1)
+    } else {
+      setFinish(true)
+    }
+
+  }
 
   return (
     <div className="App">
 
-      <div className='question-part'>
-        <p>1 / 4</p>
-        <hr />
-        <p className='question-text'>merhaba burada soru olacak</p>
-      </div>
+      {
+        finish ? 
+        (
+        <h2>
+        <p>Your score: {score}</p>
+        <small>Max score: {questions.length}</small>
+        </h2>
+        ) : (
 
-      <div className='options-part'>
-        {
-          questions[currentQuestion].options.map((option, index) => (
-            <button className='option' key={index}>{option.answer}</button>
-          ))
-        }
-      </div>
+          <>
+            <div className='question-part'>
+              <p>1 / 4</p>
+              <hr />
+              <p className='question-text'>
+                {questions[currentQuestion].question}
+              </p>
+            </div>
+
+            <div className='options-part'>
+              {
+                questions[currentQuestion].options.map((option, index) => (
+                  <button
+                    className='option'
+                    key={index}
+                    onClick={() => { nextQuestion(option.isCorrect) }}
+                  >
+                    {option.answer}
+                  </button>
+                ))
+              }
+            </div>
+          </>
+        )
+      }
 
     </div>
   );
